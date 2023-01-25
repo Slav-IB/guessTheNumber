19edd293147d294d2693b682fc3ef45ae16daded
Slavka Burgova <sburgovs@yahoo.com>
2023-01-25 14:23:34 +0000

    Update guessTheNumber.js
    Updated the code for the guess the number game to include if elseif statements

--- guessTheNumber.js
+++ guessTheNumber.js
@@ -1,13 +1,27 @@
+function guessTheNumber(){
 
-    const readline = require('readline').createInterface({input: process.stdin, output: process.stdout});
+const readline = require('readline').createInterface({input: process.stdin, output: process.stdout});
 
-    let computerGuess = Math.floor(Math.random() * 100);
-    let recursiveAsyncReadLine = function () {
-        readline.question (`Guess the number (0-100'):`, number =>
-        {
-
-        });
-    
-    }
-    recursiveAsyncReadLine()
+let computerGuess = Math.floor(Math.random() * 100);
+let playerGuess;
+let recursiveAsyncReadLine = function () {
+    readline.question ('Guess the number (0-100): ', number => {
+    guess = Number(number);
+    if(playerGuess >= 0 && playerGuess <= 100){
+        if(playerGuess === computerGuess ){
+            console.log("Well done! You guessed the number!");
+        }else if(playerGuess < computerGuess){
+            console.log("Your guess is low!");
+        }else if(playerGuess > computerGuess){
+            console.log("Your guess is higher!");
+        }else{
+            console.log("Invalid Number! Try again...");
+            recursiveAsyncReadLine();
+        }
+    }  
+    }); 
+}
+recursiveAsyncReadLine();
+}
+guessTheNumber()
 
